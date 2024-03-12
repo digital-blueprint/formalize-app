451f9fdc41ea2f7e5695ae48761239a5293f303d
Christoph Reiter <reiter.christoph@gmail.com>
2024-03-12 10:06:21 +0100

    hack: hardcode a form ID for fetching the submissions
    this makes the UI work at least. So we have a starting point for
    adjusting it for the new API.

--- src/dbp-formalize-show-registrations.js
+++ src/dbp-formalize-show-registrations.js
@@ -605,7 +605,7 @@ class ShowRegistrations extends ScopedElementsMixin(DBPLitElement) {
             }
         };
 
-        response = await this.httpGetAsync(this.entryPointUrl + '/formalize/submissions?perPage=10000', options);
+        response = await this.httpGetAsync(this.entryPointUrl + '/formalize/submissions?formIdentifier=7432af11-6f1c-45ee-8aa3-e90b3395e29c&perPage=10000', options);
         return response;
     }
 
