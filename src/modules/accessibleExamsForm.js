6e63bed914bc6efb10ae1ef8aa750cdc62ec95b8
Victoria Graf <victoria.graf@tugraz.at>
2024-11-29 14:55:52 +0100

    Add translation for accessible exams form

--- src/modules/accessibleExamsForm.js
+++ src/modules/accessibleExamsForm.js
@@ -17,19 +17,20 @@ export default class extends BaseObject {
 
 class FormalizeFormElement extends BaseFormElement {
     render() {
+        const i18n = this._i18n;
         console.log('-- Render FormalizeFormElement --');
         console.log('this.data', this.data);
         const data = this.data || {};
 
         return html`
             <form>
-                ${formElements.stringElement('subject', 'Subject', data.subject || '')}    
-                ${formElements.dateTimeElement('startDateTime', 'Start of Exam', data.startDateTime || '', true)}
-                ${formElements.dateTimeElement('endDateTime', 'End of Exam', data.endDateTime || '', true)}
-                ${formElements.stringElement('matriculationNumber', 'Matriculation number', data.matriculationNumber || '')}
-                ${formElements.stringElement('email', 'Email address', data.email || '')}
-                ${formElements.stringElement('room', 'Original room', data.room || '')}
-                ${formElements.stringElement('comment', 'Comment', data.comment || '')}
+                ${formElements.stringElement('subject', i18n.t('accessible-exams-form.subject'), data.subject || '')}    
+                ${formElements.dateTimeElement('startDateTime', i18n.t('accessible-exams-form.startDateTime'), data.startDateTime || '', true)}
+                ${formElements.dateTimeElement('endDateTime', i18n.t('accessible-exams-form.endDateTime'), data.endDateTime || '', true)}
+                ${formElements.stringElement('matriculationNumber', i18n.t('accessible-exams-form.matriculationNumber'), data.matriculationNumber || '')}
+                ${formElements.stringElement('email', i18n.t('accessible-exams-form.email'), data.email || '')}
+                ${formElements.stringElement('room', i18n.t('accessible-exams-form.room'), data.room || '')}
+                ${formElements.stringElement('comment', i18n.t('accessible-exams-form.comment'), data.comment || '')}
                 ${this.getCommonFormElements()}
             </form>
         `;
