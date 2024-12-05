dc4ab4dfbb0cb3fdd1299367c00a07f99da31c3f
Patrizio Bekerle <patrizio@bekerle.com>
2024-12-05 10:52:52 +0100

    nix: use node 22
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs; [
-      nodejs_20
+      nodejs_22
       curl
       zellij # smart terminal workspace
       lazygit # git terminal
