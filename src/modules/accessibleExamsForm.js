91a4b7b4aac7ca08aa14dd05a91cb3e60ab4f7ba
Victoria Graf <victoria.graf@tugraz.at>
2024-12-06 11:05:05 +0100

    Add custom datetime validation

--- src/modules/accessibleExamsForm.js
+++ src/modules/accessibleExamsForm.js
@@ -1,6 +1,7 @@
 import {BaseFormElement, BaseObject} from '../baseObject.js';
 import {html} from 'lit';
 import * as formElements from './formElements.js';
+import {classMap} from 'lit/directives/class-map.js';
 
 export default class extends BaseObject {
     getName() {
@@ -24,7 +25,7 @@ class FormalizeFormElement extends BaseFormElement {
 
         return html`
             <form>
-                ${formElements.stringElement('subject', i18n.t('accessible-exams-form.subject'), data.subject || '')}    
+                ${formElements.stringElement('subject', i18n.t('accessible-exams-form.subject'), data.subject || '', true)}    
                 ${formElements.dateTimeElement('startDateTime', i18n.t('accessible-exams-form.startDateTime'), data.startDateTime || '', true)}
                 ${formElements.dateTimeElement('endDateTime', i18n.t('accessible-exams-form.endDateTime'), data.endDateTime || '', true)}
                 ${formElements.stringElement('matriculationNumber', i18n.t('accessible-exams-form.matriculationNumber'), data.matriculationNumber || '')}
@@ -36,5 +37,62 @@ class FormalizeFormElement extends BaseFormElement {
                 ${this.getButtonRowHtml()}
             </form>
         `;
+    }
+
+    // TODO: Clean up duplicate code
+
+    getButtonRowHtml() {
+        return html`
+            <div class="button-row">
+                <button class="button is-secondary" type="button" @click=${this.resetForm}>Reset</button>
+                <button class="button is-primary" type="submit" ?disabled=${!this.saveButtonEnabled} @click=${this.validateAndSendSubmission}>
+                    Save
+                    <dbp-mini-spinner class="${classMap({hidden: this.saveButtonEnabled})}"></dbp-mini-spinner>
+                </button>
+            </div>
+        `;
+    }
+
+    validateAndSendSubmission(event) {
+        event.preventDefault();
+
+        // Validate required fields before proceeding
+        if (!this.validateForm()) {
+            return false;
+        }
+
+        // Validate datetime fields before proceeding
+        if (!this.validateDateTimeFields()) {
+            return false;
+        }
+
+        // TODO: Add further validations
+
+        this.sendSubmission(event);
+    }
+
+    validateDateTimeFields() {
+        // Select all input elements with the type "datetime-local"
+        const formElement = this.shadowRoot.querySelector('form');
+        const dateTimeFields = formElement.querySelectorAll('input[type="datetime-local"]');
+
+        // The minimum date has to be two weeks ahead
+        const min = Date.now() + 1209600000
+
+        // Loop through each datetime field
+        for (let field of dateTimeFields) {
+
+            // Check if the entered value is lower than the minimum value
+            if (Date.parse(field.value) < min) {
+
+                // If true, alert the user and return false to prevent form submission
+                // TODO: We will need to put those results into a div or something instead of using an alert for each single of them!
+                alert(`Please choose a date that is at least two weeks ahead in the field ${field.name}.`);
+                return false;
+            }
+        }
+
+        // If all datetime criteria are matched, return true to allow form submission
+        return true;
     }
 }
