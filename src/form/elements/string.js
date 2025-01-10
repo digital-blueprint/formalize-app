b3ace0b632d11ed8e19f1150a150ca2d2db83389
Patrizio Bekerle <patrizio@bekerle.com>
2025-01-10 09:43:47 +0100

    lint: fix import
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- src/form/elements/string.js
+++ src/form/elements/string.js
@@ -1,6 +1,5 @@
 import {html} from 'lit';
 import * as commonUtils from '@dbp-toolkit/common/utils.js';
-import {sanitizeForHtmlId} from '../utils.js';
 import {ScopedElementsMixin} from '@open-wc/scoped-elements';
 import {DbpBaseElement} from './base.js';
 
