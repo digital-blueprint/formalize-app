3512026d78f3fac8955fcbbeacbf898a4e8acf13
Christoph Reiter <reiter.christoph@gmail.com>
2025-09-05 09:45:57 +0200

    rollup: Port to getUrlOptions

--- rollup.config.js
+++ rollup.config.js
@@ -18,6 +18,7 @@ import {
     generateTLSConfig,
     getDistPath,
     getCopyTargets,
+    getUrlOptions,
 } from '@dbp-toolkit/dev-utils';
 import {createRequire} from 'module';
 
@@ -261,12 +262,7 @@ export default (async () => {
                 strictRequires: 'auto',
             }),
             json(),
-            urlPlugin({
-                limit: 0,
-                include: ['node_modules/suggestions/**/*.css', 'node_modules/select2/**/*.css'],
-                emitFiles: true,
-                fileName: 'shared/[name].[hash][extname]',
-            }),
+            urlPlugin(await getUrlOptions(pkg.name, 'shared')),
             !whitelabel &&
                 copy({
                     targets: [
